fb2f2ec8a2a7a687ec7c74c2bc8a575d9067e8e2
i-coder-robot <54929410+i-coder-robot@users.noreply.github.com>
2021-11-17 20:41:03 +0800

    account

--- account_web/handler/AccountHandler.go
+++ account_web/handler/AccountHandler.go
@@ -3,14 +3,18 @@ package handler
 import (
 	"context"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/i-coder-robot/mic-trainning-lessons/account_srv/proto/pb"
+	"github.com/i-coder-robot/mic-trainning-lessons/account_web/req"
 	"github.com/i-coder-robot/mic-trainning-lessons/account_web/res"
 	"github.com/i-coder-robot/mic-trainning-lessons/custom_error"
+	"github.com/i-coder-robot/mic-trainning-lessons/jwt_op"
 	"github.com/i-coder-robot/mic-trainning-lessons/log"
 	"google.golang.org/grpc"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 func HandleError(err error) string {
@@ -77,3 +81,87 @@ func pb2res(accountRes *pb.AccountRes) res.Account4Res {
 		Gender:   accountRes.Gender,
 	}
 }
+
+func LoginByPasswordHandler(c *gin.Context) {
+	var loginByPassword req.LoginByPassword
+	err := c.ShouldBindJSON(&loginByPassword)
+	if err != nil {
+		log.Logger.Error("LoginByPassword出错：" + err.Error())
+		c.JSON(http.StatusOK, gin.H{
+			"msg": "解析参数错误",
+		})
+		return
+	}
+	//TODO 校验手机号码格式
+	//loginByPassword.Mobile不匹配正则表达式，就报错
+	conn, err := grpc.Dial("127.0.0.1:9095", grpc.WithInsecure())
+	if err != nil {
+		log.Logger.Error("LoginByPasswordHandler 拨号出错：" + err.Error())
+		e := HandleError(err)
+		c.JSON(http.StatusOK, gin.H{
+			"msg": e,
+		})
+		return
+	}
+	client := pb.NewAccountServiceClient(conn)
+	r, err := client.GetAccountByMobile(context.Background(), &pb.MobileRequest{Mobile: loginByPassword.Mobile})
+	if err != nil {
+		log.Logger.Error("GRPC GetAccountByMobile 出错：" + err.Error())
+		e := HandleError(err)
+		c.JSON(http.StatusOK, gin.H{
+			"msg": e,
+		})
+		return
+	}
+	cheRes, err := client.CheckPassword(context.Background(), &pb.CheckPasswordRequest{
+		Password:       loginByPassword.Password,
+		HashedPassword: r.Password,
+		AccountId:      uint32(r.Id),
+	})
+	if err != nil {
+		log.Logger.Error("GRPC CheckPassword 出错：" + err.Error())
+		e := HandleError(err)
+		c.JSON(http.StatusOK, gin.H{
+			"msg": e,
+		})
+		return
+	}
+	checkResult := "登录失败"
+	if cheRes.Result {
+		checkResult = "登录成功"
+		j := jwt_op.NewJWT()
+		now := time.Now()
+		claims := jwt_op.CustomClaims{
+			StandardClaims: jwt.StandardClaims{
+				NotBefore: now.Unix(),
+				ExpiresAt: now.Add(time.Hour * 24 * 30).Unix(),
+			},
+			ID:          r.Id,
+			NickName:    r.Nickname,
+			AuthorityId: int32(r.Role),
+		}
+		token, err := j.GenerateJWT(claims)
+		if err != nil {
+			log.Logger.Error("GRPC GenerateJWT 出错：" + err.Error())
+			e := HandleError(err)
+			c.JSON(http.StatusOK, gin.H{
+				"msg": e,
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"msg":    "",
+			"result": checkResult,
+			"token":  token,
+		})
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"msg":    "",
+		"result": checkResult,
+		"token":  "",
+	})
+}
+
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+//.eyJleHAiOjE2Mzk0NjU4MzYsIm5iZiI6MTYzNjg3MzgzNiwiSUQiOjUsIk5pY2tOYW1lIjoiMTMwMDAwMDAwMDQiLCJBdXRob3JpdHlJZCI6MX0
+//.47YspOTF5kGO84KMN56ksJzC6sAcMCtqp13D00X6ZBI
